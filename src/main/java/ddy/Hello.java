be070df4835212110f7c76bb6ea94f8967589f48
dingdayong <dingdayong@21cn.com>
2025-09-21 15:19:17 +0800

    增加注释

--- src/main/java/ddy/Hello.java
+++ src/main/java/ddy/Hello.java
@@ -1,6 +1,15 @@
 package ddy;
+
+/**
+ * Hello类是一个简单的示例类，用于演示基本的Java程序结构。
+ * 该类包含一个main方法，作为程序的入口点，打印问候信息到控制台。
+ */
 public class Hello
 {
+	/**
+	 * 程序的主入口方法
+	 * @param args 命令行参数数组
+	 */
 	public static void main(String [] args)
 	{
 		System.out.println("HelloWord");
